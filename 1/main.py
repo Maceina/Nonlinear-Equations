13186797fb71acf6869f30d36a75cdd5c7698126
Your Name <you@example.com>
2020-10-13 14:14:29 +0300

    preparation done

--- 1/main.py
+++ 1/main.py
@@ -1,1 +1,13 @@
 # Project One. Number 15
+
+# 1. Isspresti netiesines lygtis
+
+# Metodai:
+# 1 - Stygu
+# 3 - Niutono(liestiniu) 
+# 5 - Skenavimo su mazejanciu zingsniu
+
+# Funkcijos:
+# f(x) = 2.19x^4 - 5.17x^3 - 7.17x^2 + 15.14x + 1.21
+# g(x) = e^(-(x/2)^2)*sin(2x); -6 <= x <= 6
+
