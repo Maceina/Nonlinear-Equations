6a8f4dbb22df3693dba2ce63094e709e53348fd1
Maceina <maceinaaudrius@gmail.com>
2020-11-27 19:20:25 +0200

    Second part not finished

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # Metodai:
 # 2 - Paprastuju iteracijua
 # 3 - Niutono(liestiniu) 
-# 5 - Skenavimo su mazejanciu zingsniu
+# 5 - Skenavimo su mazejanciu zingsnius
+
 
 # Funkcijos:
 # f(x) = 2.19x^4 - 5.17x^3 - 7.17x^2 + 15.14x + 1.21
